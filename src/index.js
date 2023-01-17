35796cfccd8542142ee2801b474dde79b477432d
dev-from-cbd <veryfatwombat@gmail.com>
2023-01-17 16:47:20 +1100

    js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 function check(str, bracketsConfig) {
   const stack = [];
+  const map = new Map(bracketsConfig);
   for (const char of str) {
-    for (const config of bracketsConfig) {
-      if (char === config[0]) {
-        stack.push(config[1]);
-      } else if (char === config[1]) {
-        if (stack.pop() !== char) {
-          return false;
-        }
-      }
+    if (map.has(char)) {
+      stack.push(map.get(char));
+    } else if (stack.length === 0 || stack.pop() !== char) {
+      return false;
     }
   }
   return stack.length === 0;
