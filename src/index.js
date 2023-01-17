24abc96af3dbb848ea141e7dad7ce3621ecb720e
dev-from-cbd <veryfatwombat@gmail.com>
2023-01-17 16:55:25 +1100

    js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-function check(str, bracketsConfig) {
-  const stack = [];
-  const openBrackets = new Set();
-  const pairs = new Map();
-
-  for (const [open, close] of bracketsConfig) {
-    openBrackets.add(open);
-    pairs.set(close, open);
+module.exports = function check(str, bracketsConfig) {
+  let openBrackets = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
   }
-
-  for (const char of str) {
-    if (openBrackets.has(char)) {
-      stack.push(char);
-    } else if (pairs.has(char)) {
-      if (stack.length === 0 || stack[stack.length - 1] !== pairs.get(char)) {
-        return false;
-      }
+  let bracketsPair = bracketsConfig.reduce((accum, pair) => {
+    accum[pair[1]] = pair[0];
+    return accum;
+  }, {});
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else if (
+      stack.length != 0 &&
+      bracketsPair[currentSymbol] === stack[stack.length - 1]
+    ) {
       stack.pop();
+    } else {
+      return false;
     }
   }
-
   return stack.length === 0;
-}
-module.exports = check;
+};
