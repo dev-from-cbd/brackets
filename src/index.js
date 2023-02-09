e9149ab3698a48eb28e9a585916ceaad6a488148
dev-from-cbd <veryfatwombat@gmail.com>
2023-02-09 17:06:01 +1030

    js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-module.exports = function checkBrackets(str, bracketsConfig) {
-  const bracketStack = [];
-  const bracketMap = new Map(bracketsConfig);
-
-  for (const char of str) {
-    if (bracketMap.has(char)) {
-      bracketStack.push(char);
-    } else if (bracketMap.values().includes(char)) {
-      if (bracketMap.get(bracketStack.pop()) !== char) {
-        return false;
-      }
+module.exports = function check(str, bracketsConfig) {
+  const open = [];
+  const close = [];
+  const pairs = {};
+  bracketsConfig.forEach((b) => {
+    open.push(b[0]);
+    close.push(b[1]);
+    pairs[b[1]] = b[0];
+  });
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (open.includes(char)) {
+      stack.push(char);
+    } else if (close.includes(char)) {
+      if (!stack.length || pairs[char] !== stack.pop()) return false;
     }
   }
-
-  return bracketStack.length === 0;
+  return !stack.length;
 };
