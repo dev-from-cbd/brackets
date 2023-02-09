7cdf459f708a1180bdc1d46bdc3299d580c01e7e
dev-from-cbd <veryfatwombat@gmail.com>
2023-02-09 17:10:53 +1030

    js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
   let config = Object.fromEntries(bracketsConfig);
   let openBrackets = Object.keys(config);
   let stack = [];
 
   for (let i = 0; i < str.length; i++) {
     if (openBrackets.includes(str[i])) {
-      stack.push(str[i]);
+      if (
+        stack.length !== 0 &&
+        str[i] === stack[stack.length - 1] &&
+        str[i] === config[stack[stack.length - 1]]
+      ) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
     } else {
-      if (stack.length === 0 || str[i] !== config[stack.pop()]) {
+      if (stack.length === 0) return false;
+      let topElement = stack[stack.length - 1];
+      if (str[i] === config[topElement]) {
+        stack.pop();
+      } else {
         return false;
       }
     }
   }
-
   return stack.length === 0;
-}
+};
