c39cb9588c277980a376506cae5b4d43d82f6608
dev-from-cbd <veryfatwombat@gmail.com>
2023-02-09 17:09:10 +1030

    js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  const open = [];
-  const close = [];
-  const pairs = {};
-  bracketsConfig.forEach((b) => {
-    open.push(b[0]);
-    close.push(b[1]);
-    pairs[b[1]] = b[0];
-  });
-  const stack = [];
+function check(str, bracketsConfig) {
+  let config = Object.fromEntries(bracketsConfig);
+  let openBrackets = Object.keys(config);
+  let stack = [];
+
   for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (open.includes(char)) {
-      stack.push(char);
-    } else if (close.includes(char)) {
-      if (!stack.length || pairs[char] !== stack.pop()) return false;
+    if (openBrackets.includes(str[i])) {
+      stack.push(str[i]);
+    } else {
+      if (stack.length === 0 || str[i] !== config[stack.pop()]) {
+        return false;
+      }
     }
   }
-  return !stack.length;
-};
+
+  return stack.length === 0;
+}
